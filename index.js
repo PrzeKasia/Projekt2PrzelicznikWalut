7d51a7458e9cf381082440c88850415d46da6d16
przeKasia1101~ <k.salach11@gmail.com>
2023-09-04 10:05:41 +0200

    correct

--- index.js
+++ index.js
@@ -8,19 +8,42 @@ document.addEventListener("DOMContentLoaded", function () {
     const currencyCode = currencySelect.value;
     const amount = parseFloat(amountInput.value);
 
-    const exchangeRate = await getExchangeRate(currencyCode);
-    const convertedAmount = (amount * exchangeRate).toFixed(2);
+    if (isNaN(amount) || amount <= 0) {
+      resultDiv.textContent = "Podaj prawidłową kwotę większą od zera.";
+      return; // Zakończ funkcję, jeśli kwota jest nieprawidłowa
+    }
 
-    resultDiv.textContent = `${amount.toFixed(
-      2
-    )} ${currencyCode} = ${convertedAmount} PLN`;
+    try {
+      const exchangeRate = await getExchangeRate(currencyCode);
+      if (exchangeRate !== undefined) {
+        const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+        resultDiv.textContent = `${amount.toFixed(
+          2
+        )} ${currencyCode} = ${convertedAmount} PLN`;
+      } else {
+        resultDiv.textContent =
+          "Nie można przeliczyć kwoty. Spróbuj ponownie później.";
+      }
+    } catch (error) {
+      console.error("Wystąpił błąd:", error);
+      resultDiv.textContent = "Wystąpił błąd podczas pobierania kursu waluty.";
+    }
   });
 
   async function getExchangeRate(currencyCode) {
-    const response = await fetch(
-      `https://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}/?format=json`
-    );
-    const data = await response.json();
-    return data.rates[0].mid;
+    try {
+      const response = await fetch(
+        `https://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}/?format=json`
+      );
+      if (!response.ok) {
+        throw new Error(`Błąd HTTP: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data.rates[0].mid;
+    } catch (error) {
+      throw error;
+    }
   }
 });
